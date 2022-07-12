587d6bb38782c3e9ceb609cbde655324aa1ef5bb
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-07-13 01:07:45 +0530

    Add alternate approach

--- pgm-resize.c
+++ pgm-resize.c
@@ -1,67 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-int main(int argc, char *argv[])
+int main()
 {
     int width, height, max_val;
+    int i, j, k, l;
     int new_width, new_height;
 
-    FILE *fo;
-    fo = fopen(argv[1], "rb");
     // Read the header
-    fscanf(fo, "P2\n%d %d\n%d\n", &width, &height, &max_val);
+    scanf("P2\n%d %d\n%d\n", &width, &height, &max_val);
 
     // Calculate the new width and height
-    if (argc > 2)
-    {
-        new_width = atoi(argv[2]);
-    }
-    else
-    {
-        new_width = width / 2;
-    }
+    new_width = 300;
     // Height is proportional to width
     new_height = height * new_width / width;
 
-    // Read the image
-    int image[height][width];
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            fscanf(fo, "%d", &image[i][j]);
+    // Use malloc to allocate memory for the source and destination data, each pixel is 1 byte
+    unsigned char *src = (unsigned char *)malloc(width * height);
+    unsigned char *dst = (unsigned char *)malloc(new_width * new_height);
+
+    // Read the data from stdin
+    for (i = 0; i < width * height; i++) {
+        scanf("%d", &src[i]);
+    }
+
+    // Resize the image by averaging the source pixels to the destination pixels
+    for (i = 0; i < new_height; i++) {
+        for (j = 0; j < new_width; j++) {
+            k = i * new_width + j;
+            l = i * width / new_height * width + j * width / new_width;
+            dst[k] = (src[l] + src[l + 1] + src[l + width] + src[l + width + 1]) / 4;
         }
     }
-    fclose(fo);
 
-    // Write the header to image
-    FILE *fp = fopen(strcat(strtok(argv[1], "."), "-resize.pgm"), "wb");
-
+    // WWrite the header to file
+    FILE *fp = fopen("resize.pgm", "w");
     fprintf(fp, "P2\n%d %d\n%d\n", new_width, new_height, max_val);
-
-    // Write the image
-    for (int i = 0; i < new_height; i++)
-    {
-        for (int j = 0; j < new_width; j++)
-        {
-            // Calculate the new pixel value
-            int new_pixel = 0;
-            for (int k = 0; k < 2; k++)
-            {
-                for (int l = 0; l < 2; l++)
-                {
-                    new_pixel += (image[i * 2 + k][j * 2 + l]) / 4;
-                }
-            }
-            if (new_pixel > max_val || new_pixel < 0)
-            {
-                new_pixel = 0;
-            }
-            fprintf(fp, "%d ", new_pixel);
-        }
-        fprintf(fp, "\n");
-    }
+    // Write the data to file result.pgm
+    for (i = 0; i < new_height * new_width; i++) {
+        fprintf(fp, "%d\n", dst[i]);
+     }
     fclose(fp);
+    // Free the data
+    free(src);
+    free(dst);
     return 0;
 }
